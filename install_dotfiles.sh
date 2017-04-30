3f19e54d0fbadf396bff273a5706d2f100885724
Rocky Gray <rocky.grayjr@gmail.com>
2017-04-29 21:49:40 -0400

    fix spacing

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,10 +4,10 @@ DOTFILE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/dotfiles
 BACKUP_SUFFIX='.backup'
 
 function help() {
-	command_name=$(basename $0)
+    command_name=$(basename $0)
     printf "usage: %s [-u]\n\t-u\tUninstall the dotfiles and restor any backups.\n" "${command_name}"
-	description="\nSymlink dotfiles from this repo to your home directory (%s).\nIf the dotfiles already exist in your home directory they will be backed up with a %s extension.\n"
-	printf "${description}" "${HOME}" "${BACKUP_SUFFIX}"
+    description="\nSymlink dotfiles from this repo to your home directory (%s).\nIf the dotfiles already exist in your home directory they will be backed up with a %s extension.\n"
+    printf "${description}" "${HOME}" "${BACKUP_SUFFIX}"
 }
 
 function main() {
@@ -28,31 +28,31 @@ function main() {
           ;;
       esac
     done
-	
-	pushd ${HOME} > /dev/null
+    
+    pushd ${HOME} > /dev/null
     $command
-	popd > /dev/null
+    popd > /dev/null
 }
 
 function install() {
     for dotfile in $(find ${DOTFILE_DIR} -maxdepth 1 | grep -v '\.$\|\.git\|dotfiles/%'); do
-		dotfile_name=".$(basename ${dotfile})"
-		if [ ! -L ${dotfile_name} ]; then
-			[ -e ${dotfile_name} ] && [ ! -e ${dotfile_name}${BACKUP_SUFFIX} ] && mv ${dotfile_name} "${dotfile_name}${BACKUP_SUFFIX}" && echo "Backed up ${dotfile_name} to ${dotfile_name}${BACKUP_SUFFIX}";
-			ln -s ${dotfile} ${dotfile_name} && echo "linked ${dotfile_name}";
-		fi
-	done
+        dotfile_name=".$(basename ${dotfile})"
+        if [ ! -L ${dotfile_name} ]; then
+            [ -e ${dotfile_name} ] && [ ! -e ${dotfile_name}${BACKUP_SUFFIX} ] && mv ${dotfile_name} "${dotfile_name}${BACKUP_SUFFIX}" && echo "Backed up ${dotfile_name} to ${dotfile_name}${BACKUP_SUFFIX}";
+            ln -s ${dotfile} ${dotfile_name} && echo "linked ${dotfile_name}";
+        fi
+    done
 
 }
 
 function uninstall() {
-	for dotfile in $(find ${DOTFILE_DIR} -maxdepth 1 | grep -v '\.$\|\.git'); do
-		dotfile_name=".$(basename ${dotfile})"
-		if [ -L ${dotfile_name} ]; then
-			[ -e ${dotfile_name} ] && rm ${dotfile_name} && echo "Uninstalled ${dotfile_name}"
-			[ -e "${dotfile_name}${BACKUP_SUFFIX}" ] && mv "${dotfile_name}${BACKUP_SUFFIX}" ${dotfile_name} && echo "Restored ${dotfile_name} from previous version"
-		fi
-	done
+    for dotfile in $(find ${DOTFILE_DIR} -maxdepth 1 | grep -v '\.$\|\.git'); do
+        dotfile_name=".$(basename ${dotfile})"
+        if [ -L ${dotfile_name} ]; then
+            [ -e ${dotfile_name} ] && rm ${dotfile_name} && echo "Uninstalled ${dotfile_name}"
+            [ -e "${dotfile_name}${BACKUP_SUFFIX}" ] && mv "${dotfile_name}${BACKUP_SUFFIX}" ${dotfile_name} && echo "Restored ${dotfile_name} from previous version"
+        fi
+    done
 }
 
 main "${@}"
