726107eb39cb64a06d9ce2f4ea8c701bbb13270c
Rocky Gray <rocky.grayjr@gmail.com>
2020-06-27 15:05:57 -0400

    fix spacing

--- install_bins.sh
+++ install_bins.sh
@@ -4,29 +4,29 @@ BIN_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/bins
 BACKUP_SUFFIX='.backup'
 
 function help() {
-    command_name=$(basename $0)
-    printf "usage: %s [-u]\n\t-u\tUninstall the bins and restor any backups.\n" "${command_name}"
-    description="\nSymlink bins from this repo to your home directory (%s).\nIf the bins already exist in your home directory they will be backed up with a %s extension.\n"
-    printf "${description}" "${HOME}/bin" "${BACKUP_SUFFIX}"
+  command_name=$(basename $0)
+  printf "usage: %s [-u]\n\t-u\tUninstall the bins and restor any backups.\n" "${command_name}"
+  description="\nSymlink bins from this repo to your home directory (%s).\nIf the bins already exist in your home directory they will be backed up with a %s extension.\n"
+  printf "${description}" "${HOME}/bin" "${BACKUP_SUFFIX}"
 }
 
 function main() {
-    command='install'
+  command='install'
 
-    while getopts hu FLAG; do
-      case $FLAG in
-        u)
-          command='uninstall'
-          ;;
-        h)
-          help
-          ;;
-        \?)
-          echo -e \\n"Option $OPTARG not allowed."
-          help
-          ;;
-      esac
-    done
+  while getopts hu FLAG; do
+    case $FLAG in
+    u)
+      command='uninstall'
+      ;;
+    h)
+      help
+      ;;
+    \?)
+      echo -e \\n"Option $OPTARG not allowed."
+      help
+      ;;
+    esac
+  done
 
   ${command}
 }
