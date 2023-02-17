1f432ddc1412b44238cbabdf7efc774a02bc08da
Rocky Gray <rgray@squareup.com>
2023-02-16 20:40:23 -0500

    remove hardcoded home directory

--- dotfiles/fzf.zsh
+++ dotfiles/fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/grocky/dotfiles/dotfiles/vim/plugged/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/grocky/dotfiles/dotfiles/vim/plugged/fzf/bin"
+if [[ ! "$PATH" == *${HOME}/dotfiles/dotfiles/vim/plugged/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}${HOME}/dotfiles/dotfiles/vim/plugged/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/grocky/dotfiles/dotfiles/vim/plugged/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/dotfiles/dotfiles/vim/plugged/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/grocky/dotfiles/dotfiles/vim/plugged/fzf/shell/key-bindings.zsh"
+source "${HOME}/dotfiles/dotfiles/vim/plugged/fzf/shell/key-bindings.zsh"
