8bacd937c0606406eee688255f4eecbfeb489cb1
Rocky Gray <rocky.grayjr@gmail.com>
2024-07-21 18:10:49 -0400

    remove powerline - install tmux powerline

--- dotfiles/fzf.zsh
+++ dotfiles/fzf.zsh
@@ -1,13 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *${HOME}/dotfiles/dotfiles/vim/plugged/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}${HOME}/dotfiles/dotfiles/vim/plugged/fzf/bin"
+if [[ ! "$PATH" == */Users/grocky/dotfiles/dotfiles/vim/plugged/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/Users/grocky/dotfiles/dotfiles/vim/plugged/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "${HOME}/dotfiles/dotfiles/vim/plugged/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "${HOME}/dotfiles/dotfiles/vim/plugged/fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
