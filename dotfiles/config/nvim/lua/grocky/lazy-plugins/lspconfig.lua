69fed49c2f27c80e5273589570aa1da561407f5f
Rocky Gray <rocky.grayjr@gmail.com>
2026-04-22 17:13:08 -0400

    (nvim): actually use cmp_lsp capabilities for LSPs

--- dotfiles/config/nvim/lua/grocky/lazy-plugins/lspconfig.lua
+++ dotfiles/config/nvim/lua/grocky/lazy-plugins/lspconfig.lua
@@ -61,9 +61,10 @@ return {
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
             "force",
-            {},
             vim.lsp.protocol.make_client_capabilities(),
             cmp_lsp.default_capabilities())
+
+        vim.lsp.config('*', { capabilities = capabilities })
 
         require("fidget").setup({})
 
